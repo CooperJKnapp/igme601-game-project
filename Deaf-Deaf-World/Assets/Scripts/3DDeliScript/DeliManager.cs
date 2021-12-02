8ba2b101c3f8ab379c569e34b4805ec30038d967
Jackson <jvs8230@rit.edu>
2021-12-01 21:22:35 -0500

    added some management for the 3d deli to switch scenes easier

---
+++ Deaf-Deaf-World/Assets/Scripts/3DDeliScript/DeliManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeliManager : MonoBehaviour
+{
+    //[Header("Pathways")]
+    private Transform player;
+    private doorExit exit;
+    private deliOrder order;
+
+    private Transform doorSpawn;
+    private Transform orderSpawn;
+
+    public bool entering;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player").transform;
+        exit = GameObject.Find("doorSpot").GetComponent<doorExit>();
+        order = GameObject.Find("orderSpot").GetComponent<deliOrder>();
+
+        doorSpawn = GameObject.Find("doorSpawn").transform;
+        orderSpawn = GameObject.Find("orderSpawn").transform;
+
+        if (entering)
+        {
+            player.position = doorSpawn.position;
+            player.rotation = doorSpawn.rotation;
+
+            exit.canExit = false;
+            order.canOrder = true;
+        }
+        else
+        {
+            player.position = orderSpawn.position;
+            player.rotation = orderSpawn.rotation;
+
+            exit.canExit = true;
+            order.canOrder = false;
+        }
+    }
+
+}
